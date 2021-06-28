10ca54c92262d8fb722ad289cda0b7e68e94ac91
preethi <fabreztina@gmail.com>
2021-06-28 13:50:39 +0530

    adding list.js file

---
+++ list.js
@@ -0,0 +1,215 @@
+   const ul = document.createElement('ul');
+    const li = document.createElement('li');
+    ul.appendChild(li);
+    li.textContent = "Semester 1";
+    document.body.appendChild(ul);  
+
+    const ul2 = document.createElement('ul') ;
+    const li2 = document.createElement('li');
+    ul2.appendChild(li2);
+    li2.textContent = "Communicative English";
+    li.appendChild(ul2);
+    const li3 = document.createElement('li');
+    li3.textContent = "Engineering Maths 1";
+    ul2.appendChild(li3);
+    const li4 = document.createElement('li');
+    li4.textContent = "Engineering Physics";
+    ul2.appendChild(li4);
+    const li5 = document.createElement('li');
+    li5.textContent = "Engineering Chemistry";
+    ul2.appendChild(li5);
+    const li6 = document.createElement('li');
+    li6.textContent = "Problem solving &Python Programming";
+    ul2.appendChild(li6);
+    const li7 = document.createElement('li');
+    li7.textContent = "Engineering Graphics";
+    ul2.appendChild(li7);
+
+
+    const ul3 = document.createElement('ul');
+    const li8 = document.createElement('li');
+    ul3.appendChild(li8);
+    li8.textContent = "semester 2";
+    document.body.appendChild(ul3);
+
+    const ul4 = document.createElement('ul');
+    const li9 = document.createElement('li');
+    ul4.appendChild(li9);
+    li9.textContent = "Technical English";
+    li8.appendChild(ul4);
+    const li10 = document.createElement('li');
+    li10.textContent = "Engineering Maths 2";
+    ul4.appendChild(li10);
+    const li11 = document.createElement('li');
+    li11.textContent = "Physics for Information Science";
+    ul4.appendChild(li11);
+    const li12 = document.createElement('li');
+    li12.textContent = "BEEM";
+    ul4.appendChild(li12);
+    const li13 = document.createElement('li');
+    li13.textContent = "Environmental Science & Engineering";
+    ul4.appendChild(li13);
+    const li14 = document.createElement('li');
+    li14.textContent = "Programming in C";
+    ul4.appendChild(li14);
+    
+
+    const ul5 = document.createElement('ul');
+    const li15 = document.createElement('li');
+    ul5.appendChild(li15);
+    li15.textContent = "semester 3";
+    document.body.appendChild(ul5);
+
+    const ul6 = document.createElement('ul');
+    const li16 = document.createElement('li');
+    ul6.appendChild(li16);
+    li16.textContent = "Digital Principle & Design";
+    li15.appendChild(ul6);
+    const li17 = document.createElement('li');
+    li17.textContent = "Discrete Mathematics";
+    ul6.appendChild(li17);
+    const li18 = document.createElement('li');
+    li18.textContent = "Data Structure";
+    ul6.appendChild(li18);
+    const li19 = document.createElement('li');
+    li19.textContent = "Object Oriented Programming";
+    ul6.appendChild(li19);
+    const li20 = document.createElement('li');
+    li20.textContent = "Communication Engineering";
+    ul6.appendChild(li20);
+    const li21 = document.createElement('li');
+    li21.textContent = "InterPersonal skills / Listening & Speaking";
+    ul6.appendChild(li21);
+
+    const ul7 = document.createElement('ul');
+    const li22 = document.createElement('li');
+    ul7.appendChild(li22);
+    li22.textContent = "semester 4";
+    document.body.appendChild(ul7);
+
+    const ul8 = document.createElement('ul');
+    const li23 = document.createElement('li');
+    ul8.appendChild(li23);
+    li23.textContent = "Probability & Queueing Theory";
+    li22.appendChild(ul8);
+    const li24 = document.createElement('li');
+    li24.textContent = "Computer Architecture";
+    ul8.appendChild(li24);
+    const li25 = document.createElement('li');
+    li25.textContent = "Database Management System";
+    ul8.appendChild(li25);
+    const li26 = document.createElement('li');
+    li26.textContent = "Design & Analysis of Algorithm";
+    ul8.appendChild(li26);
+    const li27 = document.createElement('li');
+    li27.textContent = "Operating System";
+    ul8.appendChild(li27);
+    const li28 = document.createElement('li');
+    li28.textContent = "Software Engineering";
+    ul8.appendChild(li28);
+
+    const ul9 = document.createElement('ul');
+    const li29 = document.createElement('li');
+    ul9.appendChild(li29);
+    li29.textContent = "semester 5";
+    document.body.appendChild(ul9);
+
+    const ul10 = document.createElement('ul');
+    const li30 = document.createElement('li');
+    ul10.appendChild(li30);
+    li30.textContent = "Algebra & Number Theory";
+    li29.appendChild(ul10);
+    const li31 = document.createElement('li');
+    li31.textContent = "Computer Networks";
+    ul10.appendChild(li31);
+    const li32 = document.createElement('li');
+    li32.textContent = "Microprocessor & Microcontroller";
+    ul10.appendChild(li32);
+    const li33 = document.createElement('li');
+    li33.textContent = "Theory of Computation";
+    ul10.appendChild(li33);
+    const li34 = document.createElement('li');
+    li34.textContent = "Object Oriented Analysis & Design";
+    ul10.appendChild(li34);
+    const li35 = document.createElement('li');
+    li35.textContent = "Geographic Information System";
+    ul10.appendChild(li35);
+
+
+    const ul11 = document.createElement('ul');
+    const li36 = document.createElement('li');
+    ul11.appendChild(li36);
+    li36.textContent = "semester 6";
+    document.body.appendChild(ul11);
+
+    const ul12 = document.createElement('ul');
+    const li37 = document.createElement('li');
+    ul12.appendChild(li37);
+    li37.textContent = "Internet Programming";
+    li36.appendChild(ul12);
+    const li38 = document.createElement('li');
+    li38.textContent = "Artificial Intelligence";
+    ul12.appendChild(li38);
+    const li39 = document.createElement('li');
+    li39.textContent = "Mobile computing";
+    ul12.appendChild(li39);
+    const li40 = document.createElement('li');
+    li40.textContent = "Compiler Design";
+    ul12.appendChild(li40);
+    const li41 = document.createElement('li');
+    li41.textContent = "Distributed system";
+    ul12.appendChild(li41);
+    const li42 = document.createElement('li');
+    li42.textContent = "Data Warehouse & Data Mining";
+    ul12.appendChild(li42);
+
+
+    const ul13 = document.createElement('ul');
+    const li43 = document.createElement('li');
+    ul13.appendChild(li43);
+    li43.textContent = "semester 7";
+    document.body.appendChild(ul13);
+
+    const ul14 = document.createElement('ul');
+    const li44 = document.createElement('li');
+    ul14.appendChild(li44);
+    li44.textContent = "Principles of Management";
+    li43.appendChild(ul14);
+    const li45 = document.createElement('li');
+    li45.textContent = "Cryptography & Network Security";
+    ul14.appendChild(li45);
+    const li46 = document.createElement('li');
+    li46.textContent = "Cloud Computing";
+    ul14.appendChild(li46);
+    const li47 = document.createElement('li');
+    li47.textContent = "Big Data Analytics";
+    ul14.appendChild(li47);
+    const li48 = document.createElement('li');
+    li48.textContent = "Hospital Management";
+    ul14.appendChild(li48);
+    const li49 = document.createElement('li');
+    li49.textContent = "Multi Core Architecture & Programming";
+    ul14.appendChild(li49);
+
+    const ul15 = document.createElement('ul');
+    const li50 = document.createElement('li');
+    ul15.appendChild(li50);
+    li50.textContent = "semester 8";
+    document.body.appendChild(ul15);
+
+    const ul16 = document.createElement('ul');
+    const li51 = document.createElement('li');
+    ul16.appendChild(li51);
+    li51.textContent = "Professional Ethics Engineering";
+    li50.appendChild(ul16);
+    const li52 = document.createElement('li');
+    li52.textContent = "Information Retrival Techniques";
+    ul16.appendChild(li52);
+  
+
+    
+
+    
+    
+
+
